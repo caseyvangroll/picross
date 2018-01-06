f098febfe1cec46cf218eb216bdb3d315dff4229
caseyvangroll <caseyvangroll@gmail.com>
2018-01-06 13:30:09 -0600

    initial commit

---
+++ Picross/Picross.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picross
+{
+    public partial class Picross : Form
+    {
+
+        private static int CELL_SIZE = 30;
+        private static Color O_COLOR = Color.Black;
+        private static Color X_COLOR = Color.PaleVioletRed;
+        private static Color __COLOR = Color.White;
+
+
+        private PicrossGrid grid;
+        public Picross()
+        {
+            InitializeComponent();
+            grid = new PicrossGrid(10);
+            SetUpGridView();
+        }
+
+        private void SetUpGridView()
+        {
+            picrossGridView.DataSource = grid;
+            picrossGridView.RowTemplate.Height = CELL_SIZE;
+            foreach (DataGridViewColumn d in picrossGridView.Columns)
+                d.Width = CELL_SIZE;
+
+            picrossGridView.ClientSize = new Size(grid.Columns.Count * CELL_SIZE, grid.Rows.Count * CELL_SIZE);
+            this.ClientSize = picrossGridView.Size;
+        }
+
+        private void picrossGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.Value.Equals("O"))
+                e.CellStyle.BackColor = O_COLOR;
+            else if (e.Value.Equals("X"))
+                e.CellStyle.BackColor = X_COLOR;
+            else
+                e.CellStyle.BackColor = __COLOR;
+        }
+
+        private void picrossGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            picrossGridView.ClearSelection();
+        }
+    }
+
+}
---
+++ Picross/PicrossGrid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picross
+{
+    class PicrossGrid : DataTable
+    {
+        public PicrossGrid(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Rows.Add();
+                Columns.Add();
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Rows[i][j] = "O";
+                }
+            }
+
+            Rows[0][5] = "_";
+            Rows[5][1] = "X";
+            Rows[7][5] = "X";
+            Rows[2][3] = "X";
+            Rows[2][6] = "_";
+        }
+    }
+}
